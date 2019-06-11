404efc3a6497fc24281cdc084963f19cc82270ab
gao <gao@123.com>
2019-06-11 15:36:09 +0800

    双向 一对多 cascade inverse的使用

--- src/main/java/com/woniuxy/i_one2many/AppTest2.java
+++ src/main/java/com/woniuxy/i_one2many/AppTest2.java
@@ -3,24 +3,24 @@ package com.woniuxy.i_one2many;
 import org.hibernate.Session;
 import org.junit.Test;
 
-// dept一方在维护关系，emp没有维护关系
-// 因为在Dept.hbm.xml中描述出了两张表的表结构，所以算作dept维护关系！
+// emp一方在维护关系，dept没有维护关系
+// 因为在Emp.hbm.xml中描述出了两张表的表结构，所以算作emp维护关系！
 // 所谓的维护关系，就是对外键列的值进行维护。
-public class AppTest {
+public class AppTest2 {
 	@Test
 	public void testSave() throws Exception {
 		Session s = HibernateUtils.openSession();
 		s.beginTransaction();
 		// ===============================================
 		Dept d = new Dept();
-		d.setDname("软件研发部");
+		d.setDname("开发部");
 		
 		
 		Emp e = new Emp();
-		e.setEname("比尔盖茨");
+		e.setEname("刘欢");
 		
 		Emp e2 = new Emp();
-		e2.setEname("乔布斯");
+		e2.setEname("葛优");
 		
 		
 		// #################################################################################################
@@ -28,12 +28,9 @@ public class AppTest {
 		// 让部门记住员工
 //		d.getEmps().add(e);
 //		d.getEmps().add(e2);
-		// 此时，必须手动添加员工，如果值添加部门而不添加员工，则在增加部门之后，部门还要维护关系：给员工补上部门id，可是员工压根没有入库，无法补部门id
-//			Hibernate: insert into Emp (ename, sal, hiredate) values (?, ?, ?)
-//			Hibernate: insert into Emp (ename, sal, hiredate) values (?, ?, ?)
-//			Hibernate: insert into Dept (dname) values (?)
-//			Hibernate: update Emp set dept_id=? where eid=?
-//			Hibernate: update Emp set dept_id=? where eid=?
+//		Hibernate: insert into Emp (ename, sal, hiredate, dept_id) values (?, ?, ?, ?)
+//		Hibernate: insert into Emp (ename, sal, hiredate, dept_id) values (?, ?, ?, ?)
+//		Hibernate: insert into Dept (dname) values (?)
 //		s.save(e);		// e.getClass() --> Emp --> Emp.hbm.xml
 //		s.save(e2);		// e2.getClass() --> Emp --> Emp.hbm.xml
 //		s.save(d);		// d.getClass() --> Dept --> Dept.hbm.xml  
@@ -43,17 +40,11 @@ public class AppTest {
 		// 让员工记住部门
 //		e.setDept(d);
 //		e2.setDept(d);
-		
-		// 此时发现sql语句如下：
-//		Hibernate: insert into Emp (ename, sal, hiredate) values (?, ?, ?)
-//		Hibernate: insert into Emp (ename, sal, hiredate) values (?, ?, ?)
+//		Hibernate: insert into Emp (ename, sal, hiredate, dept_id) values (?, ?, ?, ?)
+//		Hibernate: insert into Emp (ename, sal, hiredate, dept_id) values (?, ?, ?, ?)
 //		Hibernate: insert into Dept (dname) values (?)
-//		这样的sql语句，说明了，部门和员工都能入库，但是它们之间没有关系！！ 也就是说2个员工没有部门，dept_id为null。
-//		为什么会这样呢？？ 因为现在维护关系的一方是：dept在维护关系。 因为在Dept.hbm.xml中描述出了两张表的表结构，所以算作dept维护关系！
-//		想让两个表之间的数据有外键关系，必须满足2个条件:
-//			1. s.save()操作哪个对象，hibernate就会读取哪个对象所属的类，所对应的*.hbm.xml， 该hbm.xml必须描述出2张表的信息。
-//			2. s.save()操作的对象，还必须记住对方。
-		
+//		Hibernate: update Emp set ename=?, sal=?, hiredate=?, dept_id=? where eid=?
+//		Hibernate: update Emp set ename=?, sal=?, hiredate=?, dept_id=? where eid=?
 //		s.save(e);		// e.getClass() --> Emp --> Emp.hbm.xml
 //		s.save(e2);		// e2.getClass() --> Emp --> Emp.hbm.xml
 //		s.save(d);		// d.getClass() --> Dept --> Dept.hbm.xml
@@ -77,9 +68,9 @@ public class AppTest {
 //		System.out.println(dept.getEmps());
 		
 		// 获取员工，无法带出部门
-		Emp e = s.get(Emp.class, 1);  // Emp.class --> Emp --> Emp.hbm.xml
-		System.out.println(e);
-		System.out.println(e.getDept());
+//		Emp e = s.get(Emp.class, 1);  // Emp.class --> Emp --> Emp.hbm.xml
+//		System.out.println(e);
+//		System.out.println(e.getDept());
 		
 		// ===============================================
 		s.getTransaction().commit();
@@ -94,19 +85,15 @@ public class AppTest {
 		s.beginTransaction();
 		// ===============================================
 		
-		Dept dept = s.get(Dept.class, 1);
-		// 为部门追加员工
-//		Emp e = new Emp();
-//		e.setEname("多隆");
-//		dept.getEmps().add(e);
+//		Emp e = s.get(Emp.class, 2);
 //		
-//		s.save(e);
-//		s.update(dept);
-		
-		// 从部门中移出员工，注意员工还在，只不过部门id为null了
-//		Emp e = s.get(Emp.class, 3);
-//		dept.getEmps().remove(e);
-//		s.update(dept);
+//		Dept d = new Dept();
+//		d.setDname("文艺部");
+//		
+//		e.setDept(d);
+//		
+//		s.save(d);
+//		s.update(e);
 		
 		
 		// ===============================================
@@ -121,9 +108,9 @@ public class AppTest {
 		s.beginTransaction();
 		// ===============================================
 		
-		Dept dept = s.get(Dept.class, 1);
-		
-		s.delete(dept);
+		// 删除多的一方，默认不会影响一的一方。
+		Emp e = s.get(Emp.class, 2);
+		s.delete(e);
 		
 		// ===============================================
 		s.getTransaction().commit();
--- src/main/java/com/woniuxy/i_one2many/AppTest3.java
+++ src/main/java/com/woniuxy/i_one2many/AppTest3.java
@@ -3,60 +3,72 @@ package com.woniuxy.i_one2many;
 import org.hibernate.Session;
 import org.junit.Test;
 
-// dept一方在维护关系，emp没有维护关系
-// 因为在Dept.hbm.xml中描述出了两张表的表结构，所以算作dept维护关系！
-// 所谓的维护关系，就是对外键列的值进行维护。
-public class AppTest {
+// dept和emp都维护官修
+public class AppTest3 {
 	@Test
 	public void testSave() throws Exception {
 		Session s = HibernateUtils.openSession();
 		s.beginTransaction();
 		// ===============================================
 		Dept d = new Dept();
-		d.setDname("软件研发部");
+		d.setDname("开发部");
 		
 		
 		Emp e = new Emp();
-		e.setEname("比尔盖茨");
+		e.setEname("刘欢");
 		
 		Emp e2 = new Emp();
-		e2.setEname("乔布斯");
+		e2.setEname("葛优");
 		
 		
 		// #################################################################################################
 		
 		// 让部门记住员工
-//		d.getEmps().add(e);
-//		d.getEmps().add(e2);
-		// 此时，必须手动添加员工，如果值添加部门而不添加员工，则在增加部门之后，部门还要维护关系：给员工补上部门id，可是员工压根没有入库，无法补部门id
-//			Hibernate: insert into Emp (ename, sal, hiredate) values (?, ?, ?)
-//			Hibernate: insert into Emp (ename, sal, hiredate) values (?, ?, ?)
-//			Hibernate: insert into Dept (dname) values (?)
-//			Hibernate: update Emp set dept_id=? where eid=?
-//			Hibernate: update Emp set dept_id=? where eid=?
+		d.getEmps().add(e);
+		d.getEmps().add(e2);
+//		Hibernate: insert into Emp (ename, sal, hiredate, dept_id) values (?, ?, ?, ?)
+//		Hibernate: insert into Emp (ename, sal, hiredate, dept_id) values (?, ?, ?, ?)
+//		Hibernate: insert into Dept (dname) values (?)
+//		Hibernate: update Emp set dept_id=? where eid=?
+//		Hibernate: update Emp set dept_id=? where eid=?
 //		s.save(e);		// e.getClass() --> Emp --> Emp.hbm.xml
 //		s.save(e2);		// e2.getClass() --> Emp --> Emp.hbm.xml
-//		s.save(d);		// d.getClass() --> Dept --> Dept.hbm.xml  
+		s.save(d);		// d.getClass() --> Dept --> Dept.hbm.xml  
 		
 		// #################################################################################################
 		
 		// 让员工记住部门
 //		e.setDept(d);
 //		e2.setDept(d);
-		
-		// 此时发现sql语句如下：
-//		Hibernate: insert into Emp (ename, sal, hiredate) values (?, ?, ?)
-//		Hibernate: insert into Emp (ename, sal, hiredate) values (?, ?, ?)
+//		Hibernate: insert into Emp (ename, sal, hiredate, dept_id) values (?, ?, ?, ?)
+//		Hibernate: insert into Emp (ename, sal, hiredate, dept_id) values (?, ?, ?, ?)
 //		Hibernate: insert into Dept (dname) values (?)
-//		这样的sql语句，说明了，部门和员工都能入库，但是它们之间没有关系！！ 也就是说2个员工没有部门，dept_id为null。
-//		为什么会这样呢？？ 因为现在维护关系的一方是：dept在维护关系。 因为在Dept.hbm.xml中描述出了两张表的表结构，所以算作dept维护关系！
-//		想让两个表之间的数据有外键关系，必须满足2个条件:
-//			1. s.save()操作哪个对象，hibernate就会读取哪个对象所属的类，所对应的*.hbm.xml， 该hbm.xml必须描述出2张表的信息。
-//			2. s.save()操作的对象，还必须记住对方。
-		
+//		Hibernate: update Emp set ename=?, sal=?, hiredate=?, dept_id=? where eid=?
+//		Hibernate: update Emp set ename=?, sal=?, hiredate=?, dept_id=? where eid=?
 //		s.save(e);		// e.getClass() --> Emp --> Emp.hbm.xml
 //		s.save(e2);		// e2.getClass() --> Emp --> Emp.hbm.xml
 //		s.save(d);		// d.getClass() --> Dept --> Dept.hbm.xml
+		
+		
+		// #################################################################################################
+		
+		
+		// 让部门和员工互相记住对方， 发现有多余的sql语句！！ 
+//		d.getEmps().add(e);
+//		d.getEmps().add(e2);
+//		e.setDept(d);
+//		e2.setDept(d);
+//		Hibernate: insert into Emp (ename, sal, hiredate, dept_id) values (?, ?, ?, ?)
+//		Hibernate: insert into Emp (ename, sal, hiredate, dept_id) values (?, ?, ?, ?)
+//		Hibernate: insert into Dept (dname) values (?)
+//		Hibernate: update Emp set ename=?, sal=?, hiredate=?, dept_id=? where eid=?
+//		Hibernate: update Emp set ename=?, sal=?, hiredate=?, dept_id=? where eid=?
+//		Hibernate: update Emp set dept_id=? where eid=?
+//		Hibernate: update Emp set dept_id=? where eid=?
+//		s.save(e);		
+//		s.save(e2);		
+//		s.save(d);
+		
 			
 		// ===============================================
 		s.getTransaction().commit();
@@ -72,14 +84,14 @@ public class AppTest {
 		// ===============================================
 		
 		// 获取部门，带出员工
-//		Dept dept = s.get(Dept.class, 1);   // Dept.class --> Dept --> Dept.hbm.xml
+//		Dept dept = s.get(Dept.class, 3);   // Dept.class --> Dept --> Dept.hbm.xml
 //		System.out.println(dept);
 //		System.out.println(dept.getEmps());
 		
 		// 获取员工，无法带出部门
-		Emp e = s.get(Emp.class, 1);  // Emp.class --> Emp --> Emp.hbm.xml
-		System.out.println(e);
-		System.out.println(e.getDept());
+//		Emp e = s.get(Emp.class, 1);  // Emp.class --> Emp --> Emp.hbm.xml
+//		System.out.println(e);
+//		System.out.println(e.getDept());
 		
 		// ===============================================
 		s.getTransaction().commit();
@@ -94,20 +106,7 @@ public class AppTest {
 		s.beginTransaction();
 		// ===============================================
 		
-		Dept dept = s.get(Dept.class, 1);
-		// 为部门追加员工
-//		Emp e = new Emp();
-//		e.setEname("多隆");
-//		dept.getEmps().add(e);
-//		
-//		s.save(e);
-//		s.update(dept);
-		
-		// 从部门中移出员工，注意员工还在，只不过部门id为null了
-//		Emp e = s.get(Emp.class, 3);
-//		dept.getEmps().remove(e);
-//		s.update(dept);
-		
+		// 略
 		
 		// ===============================================
 		s.getTransaction().commit();
@@ -121,9 +120,12 @@ public class AppTest {
 		s.beginTransaction();
 		// ===============================================
 		
-		Dept dept = s.get(Dept.class, 1);
+		// 删除多的一方，默认不会影响一的一方。
+//		Emp e = s.get(Emp.class, 2);
+//		s.delete(e);
 		
-		s.delete(dept);
+		Dept d = s.get(Dept.class, 3);
+		s.delete(d);
 		
 		// ===============================================
 		s.getTransaction().commit();
