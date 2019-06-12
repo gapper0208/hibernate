f9da9314a495c72615b9d27c6db7c23f068edea1
gao <gao@123.com>
2019-06-12 16:21:19 +0800

    hibernate对象三种状态

---
+++ src/main/java/com/woniuxy/n_object3status/AppTest.java
@@ -0,0 +1,47 @@
+package com.woniuxy.n_object3status;
+
+import org.hibernate.Session;
+import org.junit.Test;
+
+/*
+*/
+
+public class AppTest {
+	@Test
+	public void test() throws Exception {
+		Session s = HibernateUtils.openSession();
+		s.beginTransaction();
+		// =======================================================
+		User user = new User();
+		user.setName("111");
+		// 被session管理，也有对应的记录，处于持久化状态
+		s.save(user);
+		
+		user.setName("222");
+		user.setName("333");
+		user.setName("444");
+		
+		// =======================================================
+		s.getTransaction().commit();
+		s.close();
+	}
+	
+	
+	@Test
+	public void test2() throws Exception {
+		Session s = HibernateUtils.openSession();
+		s.beginTransaction();
+		// =======================================================
+
+		User user = s.get(User.class, 4);
+		user.setName("qwerty");
+		
+		
+		// =======================================================
+		s.getTransaction().commit();
+		
+		s.close();
+		
+		
+	}
+}
